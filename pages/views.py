3b80fcea72677d187d265b7307ba12805b855c41
root <root@localhost.localdomain>
2018-07-05 19:18:30 +0530

    added contact view

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(request.user)
     return HttpResponse("<h1> Hello World </h1>")
+
+
+def contact_view(request, *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1> Contact Page </h1>")
