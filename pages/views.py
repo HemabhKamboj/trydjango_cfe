01470ec697439c65864c68d78bdc166f09147add
root <root@localhost.localdomain>
2018-07-05 19:34:49 +0530

    added templates

--- pages/views.py
+++ pages/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1> Hello World </h1>")
+    return render (request, "home.html")
 
 
 def contact_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1> Contact Page </h1>")
+    return render(request, "contact.html")
