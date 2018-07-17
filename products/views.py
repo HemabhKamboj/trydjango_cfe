6be4bd59222bf75885dab2fe9778b5cfa042a237
root <root@localhost.localdomain>
2018-07-18 01:01:21 +0530

    pure python form added

--- products/views.py
+++ products/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
-    content = {
-        'form': form
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+            
+    context = {
+        'form': my_form
     }
-    return render (request, "product/product_create.html", content)
+    return render (request, "product/product_create.html", context)
 
  
 def product_detail_view(request):
