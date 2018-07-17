acbec416277538f9b2407e9793329f4bb1179afc
root <root@localhost.localdomain>
2018-07-17 16:24:20 +0530

    generalised comtent in views.py

--- products/views.py
+++ products/views.py
@@ -5,11 +5,14 @@ from .models import Product
 
 def product_detail_view(request):
     obj= Product.objects.get(id=1)
-    content = {
-        'title' : obj.title,
-        'description': obj.description,
-        'summary': obj.summary,
-        'price': obj.price,
+    #content = {
+    #   'title' : obj.title,
+    #  'description': obj.description,
+    #    'summary': obj.summary,
+    #    'price': obj.price,
 
+    #}
+    content = {
+        'object': obj
     }
     return render (request, "product/detail.html", content)
