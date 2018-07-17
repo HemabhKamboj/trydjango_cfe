0151ecc7f8e4e4743762fada6ec6b1594bcd9f80
root <root@localhost.localdomain>
2018-07-17 16:13:23 +0530

    added product template and rendered product object in detail.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+def product_detail_view(request):
+    obj= Product.objects.get(id=1)
+    content = {
+        'title' : obj.title,
+        'description': obj.description,
+        'summary': obj.summary,
+        'price': obj.price,
+
+    }
+    return render (request, "product/detail.html", content)
