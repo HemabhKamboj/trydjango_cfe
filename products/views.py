c01187cf4698fb0b0f562ac89cefc2a2a73b3021
root <root@localhost.localdomain>
2018-07-17 22:40:54 +0530

    added option to create product by filling form in /create

--- products/views.py
+++ products/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Product
+from .forms import ProductForm
 
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    content = {
+        'form': form
+    }
+    return render (request, "product/product_create.html", content)
+
+ 
 def product_detail_view(request):
     obj= Product.objects.get(id=1)
     #content = {
