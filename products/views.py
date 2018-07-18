197499080202b8e8a657ffd2d24b9f6a9d707729
root <hkamboj2603199@gmail.com>
2018-07-19 01:28:25 +0530

    updated email

--- products/views.py
+++ products/views.py
@@ -34,15 +34,6 @@ def render_initial_data(request):
 #     }
 #     return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#     #print(request.GET)
-#     #print(request.POST)
-#     if request.method == "POST":
-#         my_new_title = request.POST.get('title')
-#         print(my_new_title)
-#         # Product.objects.create(title=my_new_title)
-#     context = {}
-#     return render(request, "products/product_create.html", context)
 
 
 def product_create_view(request):
