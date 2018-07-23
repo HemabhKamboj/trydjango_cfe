c29aad053f00755a83f8b3bba5f8435cc40a4f7d
HemabhKamboj <hkamboj2603199@gmail.com>
2018-07-24 00:29:00 +0530

    added delete form

--- products/urls.py
+++ products/urls.py
@@ -20,11 +20,14 @@ from products.views import (
     product_detail_view, 
     product_create_view, 
     render_initial_data,
-    dynamic_lookup_view
+    dynamic_lookup_view,
+    product_delete_view,
     )
 
 urlpatterns = [
+   
     path('products/<int:id>/', dynamic_lookup_view, name='product'),
+    path('products/<int:id>/delete/', product_delete_view, name='product_delete'),
 
 
 
@@ -45,6 +48,6 @@ urlpatterns = [
     path('contact/', contact_view),
     path('create/', product_create_view),
     path('initial/', render_initial_data),
-    path('product/', product_detail_view),
+    path('products/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
